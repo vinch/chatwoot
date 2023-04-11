65b6ea9e76c9f6857bf23ff59fa6a7a6609b03e7
Sojan <sojan@pepalo.com>
2023-04-11 18:13:51 +0530

    chore: fix variable

--- app/jobs/channels/whatsapp/templates_sync_job.rb
+++ app/jobs/channels/whatsapp/templates_sync_job.rb
@@ -2,6 +2,6 @@ class Channels::Whatsapp::TemplatesSyncJob < ApplicationJob
   queue_as :low
 
   def perform(whatsapp_channel)
-    whatsapp_channel.sync_templates unless channel.provider == 'default'
+    whatsapp_channel.sync_templates unless whatsapp_channel.provider == 'default'
   end
 end
