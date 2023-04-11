e367d94f4869af551b07632fddf0dde8522eba2b
Sojan <sojan@pepalo.com>
2023-04-11 17:54:03 +0530

    chore: Disable 360dialog temperorarily

--- app/jobs/channels/whatsapp/templates_sync_job.rb
+++ app/jobs/channels/whatsapp/templates_sync_job.rb
@@ -2,6 +2,6 @@ class Channels::Whatsapp::TemplatesSyncJob < ApplicationJob
   queue_as :low
 
   def perform(whatsapp_channel)
-    whatsapp_channel.sync_templates
+    whatsapp_channel.sync_templates unless channel.provider == 'default'
   end
 end
