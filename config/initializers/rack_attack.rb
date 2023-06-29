4859d5653180dd1c7e83f630c870116ddaea2d54
Vincent Battaglia <vincent.battaglia@gmail.com>
2023-06-29 10:33:11 +0200

    Increase limit to 100

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -106,7 +106,7 @@ class Rack::Attack
   end
 
   ## Prevent Conversation Bombing through multiple sessions
-  throttle('widget?website_token={website_token}&cw_conversation={x-auth-token}', limit: 5, period: 1.hour) do |req|
+  throttle('widget?website_token={website_token}&cw_conversation={x-auth-token}', limit: 100, period: 1.hour) do |req|
     req.ip if req.path_without_extentions == '/widget' && ActionDispatch::Request.new(req.env).params['cw_conversation'].blank?
   end
 end
